64b859f027e07812979aa4cdaa166b900d426ced
Mauricio Reyna <mauriciojreyna@gmail.com>
2019-01-16 15:08:19 -0500

    testing things out

--- Bonus-2/Program.cs
+++ Bonus-2/Program.cs
@@ -10,30 +10,61 @@ namespace Bonus_2
     {
         static void Main(string[] args)
         {
-            string input;
+
+            bool run = true;
+            bool fail;
+
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your birthdate. (MM/DD/YYYY)");
+            DateTime dOB;
+
+            if (fail = DateTime.TryParse(Console.ReadLine(), out dOB))
+            {
+                Console.WriteLine("Good to go!");
+            }
+            else
+            {
+                Console.WriteLine("That's not a valid date. Try again.");
+            }
+
+
+            if (DateTime.TryParse(Console.ReadLine(), out dOB))
+            {
+                TimeSpan age = DateTime.Now - dOB;
+                Console.WriteLine(name + " is {0} Years and {1} Days old!", (int)(age.Days / 365), age.Days % 365);
+            }
+
+            run = Continue();
+
+        }
+        public static bool Continue()
+        {
             bool shouldContinue;
 
-            do
+            Console.WriteLine("Do you want to continue? (y/n)");
+            string input = Console.ReadLine();
+            if (input.Equals("y", StringComparison.InvariantCultureIgnoreCase))
             {
-                Console.WriteLine("Please enter your name.");
-                string name = Console.ReadLine();
-                Console.WriteLine("Please enter your birthdate. (MM/DD/YYYY)");
-                DateTime dOB;
-                if (DateTime.TryParse(Console.ReadLine(), out dOB))
-                {
-                    TimeSpan age = DateTime.Now - dOB;
-                    Console.WriteLine(name + " is {0} Years and {1} Days old!", (int)(age.Days / 365.25), age.Days % 365.25);
-                }
-                else
-                {
-                    Console.WriteLine("That's an invalid date. Try again.");
-                }
-
-                Console.WriteLine("Continue? (y/n)");
-                input = Console.ReadLine();
-                shouldContinue = input == "y";
+                shouldContinue = true;
             }
-            while (shouldContinue);
+            else if (input.Equals("n", StringComparison.InvariantCultureIgnoreCase))
+            {
+                shouldContinue = false;
+            }
+            else
+            {
+                Console.WriteLine("Are you missing your Y and N keys? Try again.");
+                shouldContinue = Continue();
+            }
+            return shouldContinue;
         }
+
     }
 }
+
+
+
+
+
